19f09a5bb395f042b33ff2ae69078dbecdd88c79
Каймонов Владимир <kaivladimirv@gmail.com>
2017-04-19 21:51:13 +0600

    Выполнено задание #4 Работа с условиями

--- index.php
+++ index.php
@@ -107,7 +107,7 @@ $show_complete_tasks = rand(0, 1);
                     </div>
 
                     <label class="checkbox">
-                        <input id="show-complete-tasks" class="checkbox__input visually-hidden" type="checkbox">
+                        <input id="show-complete-tasks" class="checkbox__input visually-hidden" type="checkbox" <?= $show_complete_tasks == 1 ? 'checked' : '' ?>>
                         <span class="checkbox__text">Показывать выполненные</span>
                     </label>
                 </div>
@@ -115,6 +115,7 @@ $show_complete_tasks = rand(0, 1);
                 <table class="tasks">
 
                     <!--показывать эту строку, если переменная равна единице-->
+                    <? if ($show_complete_tasks == 1): ?>
                     <tr class="tasks__item task task--completed">
                         <td class="task__select">
                             <label class="checkbox task__checkbox">
@@ -127,6 +128,7 @@ $show_complete_tasks = rand(0, 1);
                         <td class="task__controls">
                         </td>
                     </tr>
+                    <? endif; ?>
 
                     <tr class="tasks__item task">
                         <td class="task__select">
